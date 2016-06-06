67c315c31cc163f0f557d0c5b1025381e3161cb5
Andres <andimarafioti@gmail.com>
2016-06-06 17:17:12 -0300

    seems to work now

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ __author__ = 'Andres'
 
 
 setup(name='pyside-tooltip',
-      version='0.1.4',
+      version='0.2',
       description='Custom tooltip extension for pyside',
       url='https://github.com/andimarafioti/pyside-tooltip',
       author='Andres Marafioti',
       author_email='andimarafioti@gmail.com',
       license='MIT',
-      packages=['pyside_tooltip'],
+      packages=['pyside_tooltip', 'pyside_tooltip/positioning'],
       install_requires=[
           'PySide',
       ],
