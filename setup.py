47bbbbaf46faaaa482a1b4b255f864337cab490e
Andres <andimarafioti@gmail.com>
2016-06-06 16:36:08 -0300

    gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ __author__ = 'Andres'
 
 
 setup(name='pyside-tooltip',
-      version='0.1.1',
+      version='0.1.2',
       description='Custom tooltip extension for pyside',
       url='https://github.com/andimarafioti/pyside-tooltip',
       author='Andres Marafioti',
