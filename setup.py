951c7afe148f0aab06798f25d0781e88e666be28
Andres <andimarafioti@gmail.com>
2016-06-06 16:12:45 -0300

    setup and init

---
+++ setup.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+from setuptools import setup
+
+__author__ = 'Andres'
+
+
+setup(name='pyside-tooltip',
+      version='0.1',
+      description='Custom tooltip extension for pyside',
+      url='https://github.com/andimarafioti/pyside-tooltip',
+      author='Andres Marafioti',
+      author_email='andimarafioti@gmail.com',
+      license='MIT',
+      packages=['pyside_tooltip'],
+      install_requires=[
+          'PySide',
+      ],
+      zip_safe=False)
